c54317212db8e27f5f90b62b16133a5b93afcb06
Vladimir Blomerus <vladimir.blomerus@gmail.com>
2024-08-31 20:13:13 +0200

    feat: add support for custom subreddit in meme viewer, remove unused json import, remove unneccessary formatted notation for url

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-import json
+
+requested_subreddit = ""
 
 app = Flask(__name__)
 
 def get_meme():
-    url = f"https://meme-api.com/gimme"
-    response = requests.get(url).json()
+    if requested_subreddit == "":
+        url = "https://meme-api.com/gimme"
+    else:
+        url = f"https://meme-api.com/gimme/{requested_subreddit}"
+    
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        url = "https://meme-api.com/gimme"
+        response = requests.get(url)
+
+    response = response.json() # Convert the response to a JSON object
+    
     subreddit = response["subreddit"]
     title = response["title"]
     url = response["postLink"]
     author = response["author"]
     ups = response["ups"]
     image = response["preview"][-1]
+
     return subreddit, title, url, author, ups, image
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    subreddit, title, url, author, ups, image = get_meme()
-    return render_template('index.html', subreddit=subreddit, title=title, url=url, author=author, ups=ups, image=image)
+    global requested_subreddit
+    if request.method == 'POST':
+        requested_subreddit = request.form['subreddit']
+
+    response_subreddit, title, url, author, ups, image = get_meme()
+    return render_template('index.html', subreddit=response_subreddit, requested_subreddit=requested_subreddit, title=title, url=url, author=author, ups=ups, image=image)
 
 if __name__ == '__main__':
     app.run(debug=True)
